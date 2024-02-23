cadad78aa1b257d1941716f6263d38941bfd9d28
vuwase <v.uwase@alustudent.com>
2024-02-23 12:11:43 -0800

    done

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""modules"""
+
+
+def pascal_triangle(n):
+    """pascal's triangle"""
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i >= 2:
+            for j in range(1, i):
+                row[j] = triangle[i-1][j-1] + triangle[i-1][j]
+        triangle.append(row)
+
+    return triangle
